38218853128405135977e3f4ca9887292ccf98a0
lchen23 <lchen43@gmail.com>
2019-10-31 14:38:51 -0700

    first commit

---
+++ model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.net = nn.Sequential(
+            nn.Linear(state_size, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 16),
+            nn.ReLU(inplace=True),
+            nn.Linear(16, action_size)
+        )
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = self.net(state)
+        return x
